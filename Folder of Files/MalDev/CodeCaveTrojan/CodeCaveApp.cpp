dbbfdac3aa9755368c99eb246e7c3bda1cabb5d2
SherwynB <112864485+SherwynB@users.noreply.github.com>
2024-11-19 16:25:54 +0800

    added app to try trojanizing
    added app to try trojanizing

---
+++ Folder of Files/MalDev/CodeCaveTrojan/CodeCaveApp.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+void normal_Function() {
+    printf("Welcome to Normal Function\n");
+}
+
+void myCode_Cave() {
+	
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+    asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
+}
+
+int main() {
+    printf("Welcome to Main.\n");
+    normal_Function();
+    myCode_Cave();  
+    printf("This is the end of the program.\n");
+    return 0;
+}
+
